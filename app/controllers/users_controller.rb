e8fadb35e64f36b889f9dec51646acba4c6e173d
martin mithi <gathitumithi9000@gmail.com>
2022-12-03 08:06:44 +0300

    commit tree 2d18ef1ddd7978241f6c2acb2002f111f6e27817

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+    def show
+        user= User.find_by(user_params)
+        render json: user, include: :items, status: :ok
+    end
+
+    private
+
+    def user_params
+        params.permit(:name, :description, :price)
+    end
 end
